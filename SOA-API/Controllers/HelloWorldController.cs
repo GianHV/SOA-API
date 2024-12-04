dcb41ec5a1f75372c3bb38db50095be17f85b9ef
GiangHV <giang4451050083@st.qnu.edu.vn>
2024-12-04 09:40:12 +0700

    Add project files.

---
+++ SOA-API/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SOA_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ApiControllerBase
+    {
+        [HttpGet]
+        public ApiResponse Get()
+        {
+            var data = "Hello world";
+            return APIResponse(data);
+        }
+    }
+}
